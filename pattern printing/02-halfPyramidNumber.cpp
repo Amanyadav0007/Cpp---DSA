d504ddd577086eb0c82124223d3f94770c02c96d
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2024-04-12 12:47:16 +0530

    Add files via upload

---
+++ pattern printing/02-halfPyramidNumber.cpp
@@ -0,0 +1,26 @@
+// Program to Print a Half-Pyramid Using Numbers.
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter value of n : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
