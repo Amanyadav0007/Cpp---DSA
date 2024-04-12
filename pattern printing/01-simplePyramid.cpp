03c32d68f6f7e4dd7495ea8dc370ae5ddc3203bd
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2024-04-12 12:34:19 +0530

    Add files via upload

--- pattern printing/01-simplePyramid.cpp
+++ pattern printing/01-simplePyramid.cpp
@@ -1,1 +1,27 @@
+// Write a c++ program for Simple Pyramid.
+// *
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+            cout<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
