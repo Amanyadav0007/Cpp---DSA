b9cb9713c2a394efd8ead289d001d4a95fa43514
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2024-04-14 07:28:44 +0530

    Add files via upload

---
+++ pattern printing/09-hollowRectangle.cpp
@@ -0,0 +1,30 @@
+// wap for Hollow Rectangle Pattern in c++.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout << "Enter the value of row : ";
+    cin >> row;
+    cout << "Enter the value of col : ";
+    cin >> col;
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == row || j == 1 || j == col)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  "; // here is 2 spaces!
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
